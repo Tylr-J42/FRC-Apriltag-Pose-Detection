1759d2beda2699eafcfec727bab2e9f032526889
Tylr-J42 <tyler.jacques00@gmail.com>
2024-12-24 15:41:50 -0500

    double camera file

--- FRC_Fiducial_Tracking/Double_Cam_PNP.py
+++ FRC_Fiducial_Tracking/Double_Cam_PNP.py
@@ -41,17 +41,23 @@ objp = np.array([[0,0,0], [-b/2, -b/2, 0], [b/2, -b/2, 0], [b/2, b/2, 0], [-b/2,
 # 2d axis array points for drawing cube overlay
 axis = np.array([[b/2, b/2, 0], [-b/2, b/2, 0], [-b/2, -b/2, 0], [b/2, -b/2, 0], [b/2, b/2, b], [-b/2, b/2, b], [-b/2, -b/2, b], [b/2, -b/2, b]], dtype=np.float32)
 
-tx3 = 0
-ty3 = 0
+cam1tx3 = 0
+cam1ty3 = 0
+cam2tx3 = 0
+cam2ty3 = 0
 
 FPS = 0
 
 # network tables + RoboRio IP
 inst = ntcore.NetworkTableInstance.getDefault()
 vision_table = inst.getTable("Fiducial")
-tag3tx = vision_table.getDoubleTopic("tag3tx").publish()
-tag3ty = vision_table.getDoubleTopic("tag3ty").publish()
+
+cam1tag3tx = vision_table.getDoubleTopic("cam1tag3tx").publish()
+cam1tag3ty = vision_table.getDoubleTopic("cam1tag3ty").publish()
+cam2tag3tx = vision_table.getDoubleTopic("cam1tag3tx").publish()
+cam2tag3ty = vision_table.getDoubleTopic("cam1tag3ty").publish()
 FPS_topic = vision_table.getDoubleTopic("fps").publish()
+
 inst.startClient4("client")
 inst.setServerTeam(2648)
 
@@ -67,44 +73,25 @@ cam.set(cv2.CAP_PROP_SHARPNESS, 3)
 cam.set(cv2.CAP_PROP_BRIGHTNESS, 0)
 cam.set(cv2.CAP_PROP_CONTRAST, 32)
 
-def tag_corners(tag_coords):
-    corners = []
+cam2 = cv2.VideoCapture(2)
 
-    for i in range(len(constants.tag_coords)):
-        x = tag_coords[i][1]
-        y = tag_coords[i][2]
-        z = tag_coords[i][3]
-        z_rotation = tag_coords[i][4]
-
-        coordinates = [[], [], [] ,[], []]
-
-        x_offset = (b/2)*math.cos(math.radians(z_rotation))
-        y_offset = (b/2)*math.sin(math.radians(z_rotation))
-        coordinates[0] = tag_coords[i][0]
-        coordinates[1] = [x-x_offset, y+y_offset, z+b/2]
-        coordinates[2] = [x+x_offset, y+y_offset, z+b/2]
-        coordinates[3] = [x+x_offset, y+y_offset, z-b/2]
-        coordinates[4] = [x-x_offset, y+y_offset, z-b/2]
-
-        corners.append(coordinates)
-
-    return corners
+cam2.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+cam2.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cam2.set(cv2.CAP_PROP_FRAME_HEIGHT, 800)
+cam2.set(cv2.CAP_PROP_FPS, 100.0)
+cam2.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1) # manual mode
+cam2.set(cv2.CAP_PROP_EXPOSURE, 60)
+cam2.set(cv2.CAP_PROP_SHARPNESS, 3)
+cam2.set(cv2.CAP_PROP_BRIGHTNESS, 0)
+cam2.set(cv2.CAP_PROP_CONTRAST, 32)
 
 def getTXTY(tvecX, tvecY, tvecZ):
     ty = np.rad2deg(math.atan(np.deg2rad(tvecY)/np.deg2rad(tvecZ))) + constants.robo_space_pose[1]
     tx = np.rad2deg(math.atan(np.deg2rad(tvecX)/np.deg2rad(tvecZ))) + constants.robo_space_pose[2]
     return tx, ty
 
-field_tag_coords = tag_corners(constants.tag_coords)
-
-def getTagCoords(tag_id):
-    return constants.tag_coords[tag_id]
-
-def connectionListener(connected, info):
-    print(info, "; Connected=%s" % connected)
-
 # create overlay on camera feed
-def display_features(image, imgpts, totalDist):
+def display_features(image, imgpts, totalDist, det):
     # making red lines around fiducial
     for i in range(0,4):
         f = i+1
@@ -121,6 +108,39 @@ def display_features(image, imgpts, totalDist):
     image = cv2.drawContours(image, [imgpts[4:]],-1,(0,0,255),3)
     image = cv2.putText(image, "#"+str(det.tag_id)+", "+str(round(totalDist, 4))+"in", (int(det.center[0]),int(det.center[1])+25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2, cv2.LINE_AA)
     return image
+
+def detection_loop(output, output_image):
+    data_array = []
+    tags_detected = []
+
+    for det in output1:
+            # if the confidence is less than 30% exclude the tag from being processed.
+            if det.decision_margin>30:
+                # points of the tag to be tracked
+                tag_points = np.array([[det.center[0], det.center[1]], [det.corners[0][0], det.corners[0][1]], [det.corners[1][0], det.corners[1][1]], [det.corners[2][0], det.corners[2][1]], [det.corners[3][0], det.corners[3][1]]], dtype=np.float32)
+
+                ret,rvecs, tvecs = cv2.solvePnP(objp, tag_points, constants.camera_matrix, constants.dist, flags=0)
+
+                # making translation and rotation vectors into a format good for networktables
+                tvecDist = tvecs.tolist()
+                rvecDeg = (rvecs*RAD2DEG).tolist()
+                for i in range(0,len(tvecDist)):
+                    tvecDist[i] = float(tvecDist[i][0])
+                for i in range(0,len(rvecDeg)):
+                    rvecDeg[i] = float(rvecDeg[i][0])
+
+                totalDist = sqrt((tvecDist[0]**2)+(tvecDist[1]**2)+(tvecDist[2]**2))
+
+                # only show display if you use --display for argparse
+                if args.display:
+                    imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, constants.camera_matrix, constants.dist)
+                    image = display_features(output_image, imgpts, totalDist, det)
+                    
+
+                data_array.append(TagObj(det.tag_id, tvecDist, rvecDeg, totalDist))
+                tags_detected.append(det.tag_id)
+    return data_array, tags_detected, image
+
 
 # setting up apriltag detection. Make sure this is OUTSIDE the loop next time
 detector = dt_apriltags.Detector(searchpath=['apriltags'],
@@ -139,62 +159,54 @@ time.sleep(0.1)
 while True:
     frame_start = time.time()
     ret, image = cam.read()
-    data_array = []
-    tags_detected = []
-    tx3 = 0
-    ty3 = 0
+    ret2, image2 = cam2.read()
+    cam1tx3 = 0
+    cam1ty3 = 0
+    cam2tx3 = 0
+    cam2ty3 = 0
 
     #detecting april tags
     tagFrame = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    tagFrame2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
     
-    output = detector.detect(tagFrame)
+    output1 = detector.detect(tagFrame)
+    output2 = detector.detect(tagFrame2)
 
     #print(output)
 
-    for det in output:
-        # if the confidence is less than 30% exclude the tag from being processed.
-        if det.decision_margin>30:
-            # points of the tag to be tracked
-            tag_points = np.array([[det.center[0], det.center[1]], [det.corners[0][0], det.corners[0][1]], [det.corners[1][0], det.corners[1][1]], [det.corners[2][0], det.corners[2][1]], [det.corners[3][0], det.corners[3][1]]], dtype=np.float32)
+    data_array1, tags_detected1, image = detection_loop(output1, image)
+    data_array2, tags_detected2, image2 = detection_loop(output2, image2)
 
-            ret,rvecs, tvecs = cv2.solvePnP(objp, tag_points, constants.camera_matrix, constants.dist, flags=0)
-
-            # making translation and rotation vectors into a format good for networktables
-            tvecDist = tvecs.tolist()
-            rvecDeg = (rvecs*RAD2DEG).tolist()
-            for i in range(0,len(tvecDist)):
-                tvecDist[i] = float(tvecDist[i][0])
-            for i in range(0,len(rvecDeg)):
-                rvecDeg[i] = float(rvecDeg[i][0])
-
-            totalDist = sqrt((tvecDist[0]**2)+(tvecDist[1]**2)+(tvecDist[2]**2))
-
-            # only show display if you use --display for argparse
-            if args.display:
-                imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, constants.camera_matrix, constants.dist)
-                image = display_features(image, imgpts, totalDist)
-
-            data_array.append(TagObj(det.tag_id, tvecDist, rvecDeg, totalDist))
-            tags_detected.append(det.tag_id)
-    
-    for i in range(len(data_array)):
-        tx, ty = getTXTY(data_array[i].tvec_x, data_array[i].tvec_y, data_array[i].tvec_z)
+    for i in range(len(data_array1)):
+        tx, ty = getTXTY(data_array1[i].tvec_x, data_array1[i].tvec_y, data_array1[i].tvec_z)
         
-        if(data_array[i].tag_id == 3):
-            tx3 = tx
-            ty3 = ty
+        if(data_array1[i].tag_id == 3):
+            cam1tx3 = tx
+            cam1ty3 = ty
+    
+    for i in range(len(data_array2)):
+        tx, ty = getTXTY(data_array2[i].tvec_x, data_array2[i].tvec_y, data_array2[i].tvec_z)
+        
+        if(data_array2[i].tag_id == 3):
+            cam2tx3 = tx
+            cam2ty3 = ty
 
     #Showing image. use --display to show image
     if args.display:
         image = cv2.putText(image, "FPS: "+str(round(FPS, 4)), (25,440), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2, cv2.LINE_AA)
-        cv2.imshow("Frame", image)
+        cv2.imshow("Camera 1", image)
+
+        image2 = cv2.putText(image2, "FPS: "+str(round(FPS, 4)), (25,440), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2, cv2.LINE_AA)
+        cv2.imshow("camera 2", image2)
 
         key = cv2.waitKey(1) & 0xFF
         if key ==ord("q"):
             break
 
-    tag3tx.set(tx3)
-    tag3ty.set(ty3)
+    cam1tag3tx.set(cam1tx3)
+    cam1tag3ty.set(cam1ty3)
+    cam2tag3tx.set(cam2tx3)
+    cam2tag3ty.set(cam2ty3)
 
     FPS_topic.set(FPS)
 
